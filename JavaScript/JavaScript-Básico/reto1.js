f4093bbf177ea6291ced8fe67b945ad47b477fd2
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2021-08-11 22:47:48 -0500

    update reto1

--- JavaScript/JavaScript-Básico/reto1.js
+++ JavaScript/JavaScript-Básico/reto1.js
@@ -2,23 +2,23 @@ var piedra = "Piedra";
 var papel = "Papel";
 var tijera = "Tijera";
 
-var resultado = function(user, cpu) 
+var resultado = function(user, pc) 
 {
-    if(user != cpu) 
+    if(user != pc) 
     {
-        if(user === piedra && cpu === tijera) 
+        if(user === piedra && pc === tijera) 
         {
             console.log("El usuario ganó con " + piedra)
-        }else if (user === papel && cpu === piedra) 
+        }else if (user === papel && pc === piedra) 
         {
             console.log("El usuario ganó con " + papel)
-        }else if (user === tijera && cpu === papel) 
+        }else if (user === tijera && pc === papel) 
         {
             console.log("El usuario ganó con " + tijera)
         }else {
             console.log("Perdiste")
         }
-    }else if (user === cpu) 
+    }else if (user === pc) 
     {
         console.log("Empate")
     }
@@ -26,3 +26,29 @@ var resultado = function(user, cpu)
 
 ///
 resultado(piedra, tijera)
+
+
+
+var usuario = 2;
+var pc = 2;
+
+function juego(usuario, pc)
+{
+    switch(true)
+    {
+        case (usuario === pc):
+            console.log("Empate");
+            break;
+        case (usuario === 2 && pc === 1):
+            console.log("Ganaste");
+            break;
+        case (usuario === 0 && pc === 2):
+            console.log("Ganaste");
+            break;
+        case (usuario === 1 && pc === 0):
+            console.log("Ganaste");
+            break;
+        default: 
+            console.log("Perdiste");
+    }
+}
