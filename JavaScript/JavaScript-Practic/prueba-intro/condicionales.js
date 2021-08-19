7027e0f0932f9182e30ae7785fd7025adbc164b6
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2021-08-18 22:36:24 -0500

    update file JavaScript/JavaScript-P

--- JavaScript/JavaScript-Practic/prueba-intro/condicionales.js
+++ JavaScript/JavaScript-Practic/prueba-intro/condicionales.js
@@ -17,3 +17,17 @@ function tipoDeSuscripcion(suscripcion) {
 	}
 }
 
+// Solo con if
+const tipoDeSuscripcion="Basic";
+
+if(tipoDeSuscripcion=="Free")
+console.log("Solo puedes tomar los cursos gratis");
+
+if(tipoDeSuscripcion=="Basic")
+console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+
+if(tipoDeSuscripcion=="Expert")
+console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+
+if(tipoDeSuscripcion=="Expert+")
+console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
