b2daeec2e266804e0b5f6ca400fd8f0a71e7f28a
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2021-08-18 23:02:31 -0500

    update file JavaScript/JavaScript-P

---
+++ JavaScript/JavaScript-Practic/prueba-intro/ciclos.js
@@ -0,0 +1,9 @@
+function suma() {
+    let resultado;
+    do {
+        resultado = parseInt(prompt('¿Cuál es el resultado de "2 + 2"? '));
+    } while ( resultado !== 4 );
+    if (resultado === 4);
+    return console.log('¡Perfecto!');
+}
+suma();
