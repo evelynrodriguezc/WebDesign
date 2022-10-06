5882d6ef9d3b336c2e2f239e55468eaa05e7dd37
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2022-10-06 00:53:23 -0500

    example manupulating DOM

---
+++ JavaScript/index.js
@@ -0,0 +1,14 @@
+
+const title = document.querySelector('h1');
+const p = document.querySelector('p');
+const pclass = document.querySelector('.pclass');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+console.log({
+    title,
+    p,
+    pclass,
+    pid,
+    input,
+});
